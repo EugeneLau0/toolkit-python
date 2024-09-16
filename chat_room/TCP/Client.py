d96d4a29181c2a0aa7714f03efa446064e07146c
yuanjun <eugenelau@foxmail.com>
2024-09-16 10:55:27 +0800

    update tcp server

--- chat_room/TCP/Client.py
+++ chat_room/TCP/Client.py
@@ -3,5 +3,7 @@ from socket import socket, AF_INET, SOCK_STREAM
 if __name__ == '__main__':
     s = socket(AF_INET, SOCK_STREAM)
     s.connect(('localhost', 20000))
-    s.send(b'Hello')
-    s.recv(8192)
+    s.send('Hello'.encode('utf-8'))
+    # 指定接收的端口
+    return_msg = s.recv(8192)
+    print(return_msg.decode('utf-8'))
