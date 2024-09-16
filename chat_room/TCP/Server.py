b78618b1a336458391f3398cda63a7c57a0bad11
yuanjun <eugenelau@foxmail.com>
2024-09-16 10:55:27 +0800

    update tcp server

--- chat_room/TCP/Server.py
+++ chat_room/TCP/Server.py
@@ -7,14 +7,14 @@ class EchoHandler(BaseRequestHandler):
         while True:
             msg = self.request.recv(8192)
             if not msg:
-                print('got msg: ', msg)
                 break
-            self.request.send(msg)
+            print('got msg: ', msg.decode('utf-8'))
+            self.request.send('got it: {}'.format(msg.decode('utf-8')).encode('utf-8'))
 
 
 if __name__ == '__main__':
     # 定义端口port：20000
     # 这是一种单线程的方式
     serv = TCPServer(('', 20000), EchoHandler)
-    print('server started')
+    print('server started:')
     serv.serve_forever()
