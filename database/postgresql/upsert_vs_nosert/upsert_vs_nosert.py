e3d00328e4a5beee6ecf422e8da011f5bf9c4e6d
yuanjun <eugenelau@foxmail.com>
2024-10-08 11:01:36 +0800

    更新程序及ignore

--- database/postgresql/upsert_vs_nosert/upsert_vs_nosert.py
+++ database/postgresql/upsert_vs_nosert/upsert_vs_nosert.py
@@ -17,15 +17,17 @@ config = load_db_config()
 if __name__ == '__main__':
     init_db(config)
     items = list()
-    count = 1000
+    count = 200
     for i in range(1, count + 1):
         items.append(business.create_inventory_item(i))
     print('开始初始化数据：')
     for i in range(1, count + 1):
         business.insert_inventory_item(config, items)
     print('初始化数据完成！')
-    print('开始交叉冲突更新：')
-    for i in range(1, count + 1):
+    print(f'批量更新【{count}】条记录，开始交叉冲突更新：')
+    for i in range(1, 11):
         first_time = business.insert_inventory_item(config, items)
         second_time = business.upsert_inventory_item(config, items)
-        print(f'第{i}次insert&update耗时：{first_time}，upsert耗时：{second_time}，前者比后者快：{first_time - second_time}秒')
+        print(
+            f'第{i}次insert&update耗时：{first_time}，upsert耗时：{second_time}，前者比后者快：{-1 * round(first_time - second_time, 2)}秒')
+    print('更新完成！')
